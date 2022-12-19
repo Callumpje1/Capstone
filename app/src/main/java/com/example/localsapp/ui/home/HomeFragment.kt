da3a7b143e4b1062f6f85ac0c557b33f97375c85
svadkoc <SvadBellum1997!>
2022-12-19 12:41:22 +0100

    Maps: added map to homefragment

--- app/src/main/java/com/example/localsapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/localsapp/ui/home/HomeFragment.kt
@@ -1,42 +1,84 @@
 package com.example.localsapp.ui.home
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.localsapp.databinding.FragmentHomeBinding
+import com.example.localsapp.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
 
-class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
+class HomeFragment : Fragment(), OnMapReadyCallback {
+    var mapView: MapView? = null
+    var map: GoogleMap? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+    ): View? {
+        val v: View = inflater.inflate(R.layout.fragment_home, container, false)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        // Gets the MapView from the XML layout and creates it
+        mapView = v.findViewById<View>(R.id.mapview) as MapView
+        mapView!!.onCreate(savedInstanceState)
+        mapView!!.getMapAsync(this)
+        return v
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        map!!.uiSettings.isMyLocationButtonEnabled = false
+        if (context?.let {
+                ActivityCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            } != PackageManager.PERMISSION_GRANTED && context?.let {
+                ActivityCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            } != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        map!!.isMyLocationEnabled = true
+
+        map!!.moveCamera(
+            CameraUpdateFactory.newLatLng(
+                LatLng(
+                    25.9,
+                    30.15
+                )
+            )
+        )
+    }
+
+    override fun onResume() {
+        mapView!!.onResume()
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView!!.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView!!.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView!!.onLowMemory()
     }
 }
